5e6a346f209b709c540a5e5722b9b611710ab4a5
Ha1TuaN <vuhaituan100903@gmail.com>
2024-04-26 17:35:22 +0700

    'abc'

--- app/src/main/java/com/example/foodorderapp/view/ProfileFragment.java
+++ app/src/main/java/com/example/foodorderapp/view/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.example.foodorderapp.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +16,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button logoutButton = rootView.findViewById(R.id.button_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Xóa tất cả các hoạt động trước đó và đặt LoginActivity làm hoạt động cực đỉnh mới
+                getContext().startActivity(intent);
+            }
+        });
+        return rootView;
     }
 }
